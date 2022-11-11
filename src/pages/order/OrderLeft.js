7c0356dd2bb82d90d8ab84bc1e3b081ea2afa9bb
KimJongEon <kjo4134@gmail.com>
2022-11-11 11:06:23 +0900

    총 결제 금액 구현

--- src/pages/order/OrderLeft.js
+++ src/pages/order/OrderLeft.js
@@ -1,5 +1,5 @@
 /* eslint-disable */ // 터미널에 경고 메세지 안뜨게 해줌
-import React from 'react';
+import {React, useState} from 'react';
 import './orderLeft.css';
 import { Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,95 +8,103 @@ import { pushProductCount, changeProductCount } from '../../store.js';
 
 
 function OrderLeft() {
+    let productCount = useSelector((state) => { return state.productCount });
 
-    let productCount = useSelector((state) => { return state.productCount })
+    let totalPrice = productCount.map((x)=>{
+        return (x.count * x.productPrice);
+    }).reduce((a, b)=> a + b, 0);
 
     // Redux : store의 state 변경하기 위한 함수 - useDispatch
-    let dispatch = useDispatch()
+    let dispatch = useDispatch();
 
+    
     return (
-        <div className="orderLeft">
-            {/* {console.log(productCount)} */}
-            <Table>
-                <thead>
-                    <tr>
-                        <th>상품 이름</th>
-                        <th>수량</th>
-                        <th>가격</th>
-                        <th>총 금액</th>
-                    </tr>
-                </thead>
+        <>
+            <div className="orderLeft">
+                {/* {console.log(productCount)} */}
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>상품 이름</th>
+                            <th>수량</th>
+                            <th>가격</th>
+                            <th>총 금액</th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                    {/* 조건문 필요 */}
-                    {/* count가 1이상 일때 행 추가 */}
-                    {/* 한 번 더 클릭하면 count만 +1 */}
+                    <tbody>
+                        {/* 조건문 필요 */}
+                        {/* count가 1이상 일때 행 추가 */}
+                        {/* 한 번 더 클릭하면 count만 +1 */}
 
-                    {/* map 반복문 사용 */}
-                    {
-                        productCount.map((a, i) => {
-                            return (
-                                <tr key={i}>
-                                    {/* {console.log(productCount[i])} */}
-                                    <td>{productCount[i].productName}</td>
-                                    <td>
-                                        {/* 수량 마이너스 버튼 */}
-                                        <Button variant="primary" onClick={() => {
-                                            let productIdx = productCount[i].productIdx
-                                            let productCountData = productCount[i].count
+                        {/* map 반복문 사용 */}
+                        {
+                            productCount.map((x, i) => {
+                                return (
+                                    <tr key={i}>
+                                        {/* {console.log(productCount[i])} */}
+                                        <td>{productCount[i].productName}</td>
+                                        <td>
+                                            {/* 수량 마이너스 버튼 */}
+                                            <Button variant="primary" onClick={() => {
+                                                let productIdx = productCount[i].productIdx
+                                                let productCountData = productCount[i].count
 
-                                            if (productCountData > 1) {
+                                                if (productCountData > 1) {
+                                                    let productCountUpdate = productCount.map((x) =>
+                                                        x.productIdx === productIdx
+                                                            ? { ...x, count: productCountData -= 1 }
+                                                            : x
+                                                    )
+                                                    dispatch(changeProductCount(productCountUpdate))
+                                                }
+                                            }}>-</Button>
+
+                                            {/* 수량 */}
+                                            {productCount[i].count}
+
+                                            {/* 수량 플러스 버튼 */}
+                                            <Button variant="primary" onClick={() => {
+                                                let productIdx = productCount[i].productIdx
+                                                let productCountData = productCount[i].count
+
                                                 let productCountUpdate = productCount.map((x) =>
+                                                    // console.log({...x})
                                                     x.productIdx === productIdx
-                                                        ? { ...x, count: productCountData -= 1 }
+                                                        ? { ...x, count: productCountData += 1 }
                                                         : x
                                                 )
                                                 dispatch(changeProductCount(productCountUpdate))
-                                            }
-                                        }}>-</Button>
+                                            }}>+</Button>
+                                        </td>
 
-                                        {/* 수량 */}
-                                        {productCount[i].count}
+                                        {/* 상품 금액 */}
+                                        <td>{productCount[i].productPrice}</td>
 
-                                        {/* 수량 플러스 버튼 */}
-                                        <Button variant="primary" onClick={() => {
-                                            let productIdx = productCount[i].productIdx
-                                            let productCountData = productCount[i].count
-
-                                            let productCountUpdate = productCount.map((x) =>
-                                                // console.log({...x})
-                                                x.productIdx === productIdx
-                                                    ? { ...x, count: productCountData += 1 }
-                                                    : x
-                                            )
-                                            dispatch(changeProductCount(productCountUpdate))
-                                        }}>+</Button>
-                                    </td>
-                                    
-                                    {/* 상품 금액 */}
-                                    <td>{productCount[i].productPrice}</td>
-
-                                    {/* 상품 총 금액 */}
-                                    <td>{productCount[i].count * productCount[i].productPrice}</td>
-                                    
-                                </tr>
-                            ); //return END
-                        })
-                    }
-                </tbody>
-            </Table>
+                                        {/* 상품 총 금액 */}
+                                        <td>{productCount[i].count * productCount[i].productPrice}</td>
+                                    </tr>
+                                ); //return END
+                            })
+                        }
+                    </tbody>
+                </Table>
                 
-            <h1>합계 테이블</h1>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>받은 금액</th>
-                        <th>거스름돈</th>
-                        <th>합계</th>
-                    </tr>
-                </thead>
-            </Table>
-        </div>
+                <h2>총 결제 금액 : {totalPrice}</h2>
+
+                {/* <h1>합계 테이블</h1>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>받은 금액</th>
+                            <th>거스름돈</th>
+                            <th>합계</th>
+                        </tr>
+                    </thead>
+                </Table> */}
+            </div>
+
+        </>
     ); //return End
 }
 
