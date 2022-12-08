98e9bdea407cced7b507ad9e451b66903208e9c4
KimJongEon <kjo4134@gmail.com>
2022-12-08 10:03:16 +0900

    orderHistory axios 변경
    App.js -> Sidebar.js 에서 orderHistory Link클릭시 axios 실행

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,9 +3,14 @@ import React from 'react';
 import './sidebar.css';
 import { BsFillBasket3Fill, BsListUl, BsArrowRightSquareFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { changeOrderHistory } from './../../store.js';
 
-
-function Sidebar(props){
+function Sidebar(props) {
+    // Redux : store의 state 변경하기 위한 함수 - useDispatch
+    let dispatch = useDispatch();
+    
     return (
         <div className="sidebar">
             <div className="sidebarWrapper">
@@ -13,19 +18,35 @@ function Sidebar(props){
                     <h3 className="sidebarTitle">주문 및 관리</h3>
                     <ul className="sidebarList">
                         <li className="sidebarListItem">
-                            <BsFillBasket3Fill/>
+                            <BsFillBasket3Fill />
                             <Link to="/">주문</Link>
                             {/* <Link onClick={()=>{ props.navigate('/') }}>Home</Link> */}
                         </li>
 
                         <li className="sidebarListItem">
-                            <BsListUl/> 
-                            <Link to="/orderHistory">주문내역</Link>
+                            <BsListUl />
+                            <Link to="/orderHistory" onClick={() => {
+                                console.log("test")
+
+                                axios.get('/api/orderHistory')
+                                    .then(orderHistory => {
+
+                                        if (orderHistory.data) {
+                                            console.log(orderHistory.data);
+                                            dispatch(changeOrderHistory(orderHistory.data));
+                                        } else {
+                                            console.log("orderHistory 데이터 없음")
+                                        }
+                                    })
+
+                            }}>
+
+                                주문내역</Link>
                             {/* <Link onClick={()=>{ props.navigate('/order') }}>Order</Link> */}
                         </li>
 
                         <li className="sidebarListItem">
-                            <BsArrowRightSquareFill/>
+                            <BsArrowRightSquareFill />
                             <Link to="/menuManagement">메뉴관리</Link>
                         </li>
 
